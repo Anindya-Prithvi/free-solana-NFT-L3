e50446bec1820d620b6afc7a0732ea9521143373
Andy <anindya20024@iiitd.ac.in>
2022-01-14 16:02:11 +0530

    airdropping func

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const secretKey = newPair._keypair.secretKey;
 //Step3 --Get wallet balance
 const getWalletBalance = async () => {
 	try {
-		const connection = new Connection(clusterApiUrl("devnet"),"confirmed");
+		const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 		const myWallet = await Keypair.fromSecretKey(secretKey);
 		const walletBalance = await connection.getBalance(new PublicKey(myWallet.publicKey));
 		console.log('=> For wallet address ${publicKey}');
@@ -28,5 +28,22 @@ const getWalletBalance = async () => {
 		console.log(err);
 	}
 };
+
+//Step4 --Getting SOL
+const airDropSol = async () => {
+	try {
+		const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+		const walletKeyPair = await Keypair.fromSecretKey(secretKey);
+		console.log('Airdropping 2 sol');
+		const fromAirDropSignature = await connection.requestAirdrop(
+			new PublicKey(walletKeyPair.publicKey),
+			2 * LAMPORTS_PER_SOL
+		);
+		await connection.confirmTransaction(fromAirDropSignature);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
 
 
